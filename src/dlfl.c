1c1d96153efae715090802c7d1a4436322f1330d
Pichulin Dmitrii <deem@deem.ru>
2015-10-31 15:06:57 +0300

    style

--- src/dlfl.c
+++ src/dlfl.c
@@ -4,15 +4,13 @@
 #pragma comment( lib, "kernel32.lib")
 #pragma comment( lib, "winhttp.lib")
 
-#define WINHTTP_TIMEOUT 9999
-#define WINHTTP_CHUNK   1280
-
 void memzero( void * mem, size_t size ) // anti memset
 {
     do
     {
         ( (volatile char *)mem )[--size] = 0;
-    } while( size );
+    }
+    while( size );
 }
 
 void memcopy( void * dst, void * src, size_t size ) // anti memcpy
@@ -20,7 +18,8 @@ void memcopy( void * dst, void * src, size_t size ) // anti memcpy
     do
     {
         ( (volatile char *)dst )[--size] = ( (volatile char *)src )[--size];
-    } while( size );
+    }
+    while( size );
 }
 
 unsigned strlength( char * str ) // anti strlen
@@ -36,6 +35,10 @@ unsigned strlength( char * str ) // anti strlen
     return length;
 }
 
+#define WINHTTP_TIMEOUT 9999
+#define WINHTTP_CHUNK   1280
+#define WINHHTP_AGENT   L"WinHTTP (github/deemru/dlfl)"
+
 typedef struct
 {
     LPCWSTR url;
@@ -44,6 +47,7 @@ typedef struct
     INTERNET_PORT port;
     BOOL isSSL;
 
+    LPCWSTR filename;
     BYTE bbChunk[WINHTTP_CHUNK];
     DWORD dwChunk;
     DWORD dwLoaded;
@@ -55,8 +59,6 @@ typedef struct
 }
 winhttp_handler;
 
-#define WINHHTP_AGENT L"WinHTTP (github/deemru/dlfl)"
-
 DWORD winhttp_exec( winhttp_handler * h )
 {
     BOOL isOK = FALSE;
@@ -64,7 +66,9 @@ DWORD winhttp_exec( winhttp_handler * h )
     if( h->hRequest )
         goto download;
 
-    h->hSession = WinHttpOpen( WINHHTP_AGENT, WINHTTP_ACCESS_TYPE_NO_PROXY, WINHTTP_NO_PROXY_NAME, WINHTTP_NO_PROXY_BYPASS, 0 );
+    h->hSession = WinHttpOpen( WINHHTP_AGENT, WINHTTP_ACCESS_TYPE_NO_PROXY,
+                               WINHTTP_NO_PROXY_NAME, WINHTTP_NO_PROXY_BYPASS,
+                               0 );
 
     if( !h->hSession )
         goto end;
@@ -74,25 +78,29 @@ DWORD winhttp_exec( winhttp_handler * h )
     if( !h->hConnect )
         goto end;
 
-    h->hRequest = WinHttpOpenRequest( h->hConnect, L"GET", h->url_get, NULL, WINHTTP_NO_REFERER, WINHTTP_DEFAULT_ACCEPT_TYPES, h->isSSL ? WINHTTP_FLAG_SECURE : 0 );
+    h->hRequest = WinHttpOpenRequest( h->hConnect, L"GET", h->url_get, NULL,
+                                      WINHTTP_NO_REFERER,
+                                      WINHTTP_DEFAULT_ACCEPT_TYPES,
+                                      h->isSSL ? WINHTTP_FLAG_SECURE : 0 );
 
     if( !h->hRequest )
         goto end;
 
-    isOK = WinHttpSetTimeouts( h->hRequest, 0, WINHTTP_TIMEOUT, WINHTTP_TIMEOUT, WINHTTP_TIMEOUT );
+    isOK = WinHttpSetTimeouts( h->hRequest, 0, WINHTTP_TIMEOUT, WINHTTP_TIMEOUT,
+                               WINHTTP_TIMEOUT );
 
     if( !isOK )
         goto end;
 
-    // PROXY-CODE
     {
         WINHTTP_PROXY_INFO ProxyInfo;
 
         isOK = FALSE;
+
         {
             WINHTTP_CURRENT_USER_IE_PROXY_CONFIG ProxyConfig;
 
-            memzero( &ProxyConfig, sizeof( WINHTTP_CURRENT_USER_IE_PROXY_CONFIG ) );
+            memzero( &ProxyConfig, sizeof( ProxyConfig ) );
             ProxyConfig.fAutoDetect = TRUE;
 
             if( WinHttpGetIEProxyConfigForCurrentUser( &ProxyConfig ) )
@@ -103,7 +111,8 @@ DWORD winhttp_exec( winhttp_handler * h )
                     ProxyInfo.lpszProxy = ProxyConfig.lpszProxy;
                     ProxyInfo.lpszProxyBypass = ProxyConfig.lpszProxyBypass;
 
-                    if( WinHttpSetOption( h->hRequest, WINHTTP_OPTION_PROXY, &ProxyInfo, sizeof( ProxyInfo ) ) )
+                    if( WinHttpSetOption( h->hRequest, WINHTTP_OPTION_PROXY,
+                                          &ProxyInfo, sizeof( ProxyInfo ) ) )
                         isOK = TRUE;
                 }
 
@@ -129,12 +138,15 @@ DWORD winhttp_exec( winhttp_handler * h )
 
             memzero( &AutoProxyOptions, sizeof( WINHTTP_AUTOPROXY_OPTIONS ) );
             AutoProxyOptions.dwFlags = WINHTTP_AUTOPROXY_AUTO_DETECT;
-            AutoProxyOptions.dwAutoDetectFlags = WINHTTP_AUTO_DETECT_TYPE_DHCP | WINHTTP_AUTO_DETECT_TYPE_DNS_A;
+            AutoProxyOptions.dwAutoDetectFlags = WINHTTP_AUTO_DETECT_TYPE_DHCP |
+                                                 WINHTTP_AUTO_DETECT_TYPE_DNS_A;
             AutoProxyOptions.fAutoLogonIfChallenged = TRUE;
 
-            if( WinHttpGetProxyForUrl( h->hSession, h->url, &AutoProxyOptions, &ProxyInfo ) )
+            if( WinHttpGetProxyForUrl( h->hSession, h->url, &AutoProxyOptions,
+                                       &ProxyInfo ) )
             {
-                if( WinHttpSetOption( h->hRequest, WINHTTP_OPTION_PROXY, &ProxyInfo, sizeof( ProxyInfo ) ) )
+                if( WinHttpSetOption( h->hRequest, WINHTTP_OPTION_PROXY,
+                                      &ProxyInfo, sizeof( ProxyInfo ) ) )
                     isOK = TRUE;
             }
             else
@@ -155,7 +167,8 @@ DWORD winhttp_exec( winhttp_handler * h )
             goto end;
     }
 
-    isOK = WinHttpSendRequest( h->hRequest, WINHTTP_NO_ADDITIONAL_HEADERS, 0, WINHTTP_NO_REQUEST_DATA, 0, 0, 0 );
+    isOK = WinHttpSendRequest( h->hRequest, WINHTTP_NO_ADDITIONAL_HEADERS, 0,
+                               WINHTTP_NO_REQUEST_DATA, 0, 0, 0 );
 
     if( !isOK )
         goto end;
@@ -166,8 +179,13 @@ DWORD winhttp_exec( winhttp_handler * h )
         goto end;
 
     {
-        DWORD dwValue, dwSize = sizeof( dwValue );
-        isOK = WinHttpQueryHeaders( h->hRequest, WINHTTP_QUERY_STATUS_CODE | WINHTTP_QUERY_FLAG_NUMBER, WINHTTP_HEADER_NAME_BY_INDEX, &dwValue, &dwSize, WINHTTP_NO_HEADER_INDEX );
+        DWORD dwValue;
+        DWORD dwSize = sizeof( dwValue );
+        
+        isOK = WinHttpQueryHeaders( h->hRequest, WINHTTP_QUERY_STATUS_CODE |
+                                    WINHTTP_QUERY_FLAG_NUMBER,
+                                    WINHTTP_HEADER_NAME_BY_INDEX, &dwValue,
+                                    &dwSize, WINHTTP_NO_HEADER_INDEX );
 
         if( !isOK )
             goto end;
@@ -179,18 +197,19 @@ DWORD winhttp_exec( winhttp_handler * h )
             goto end;
         }
 
-        isOK = WinHttpQueryHeaders( h->hRequest, WINHTTP_QUERY_CONTENT_LENGTH | WINHTTP_QUERY_FLAG_NUMBER, WINHTTP_HEADER_NAME_BY_INDEX, &dwValue, &dwSize, WINHTTP_NO_HEADER_INDEX );
+        isOK = WinHttpQueryHeaders( h->hRequest, WINHTTP_QUERY_CONTENT_LENGTH |
+                                    WINHTTP_QUERY_FLAG_NUMBER,
+                                    WINHTTP_HEADER_NAME_BY_INDEX, &dwValue,
+                                    &dwSize, WINHTTP_NO_HEADER_INDEX );
 
-        if( !isOK )
-            h->dwTotal = 0;
-        else
+        if( isOK )
             h->dwTotal = dwValue;
     }
-
 
 download:
 
     h->dwChunk = sizeof( h->bbChunk );
+
     isOK = WinHttpReadData( h->hRequest, h->bbChunk, h->dwChunk, &h->dwChunk );
 
     if( !isOK )
@@ -204,22 +223,22 @@ download:
 end:
 
     {
-        DWORD dwTemp = isOK ? S_OK : GetLastError();
+        DWORD dwRetCode = isOK ? S_OK : GetLastError();
 
         if( h->hRequest ) WinHttpCloseHandle( h->hRequest );
         if( h->hConnect ) WinHttpCloseHandle( h->hConnect );
         if( h->hSession ) WinHttpCloseHandle( h->hSession );
 
-        return dwTemp;
+        return dwRetCode;
     }
 }
 
-unsigned str2num( wchar_t * str )
+unsigned wstr2num( wchar_t * wstr )
 {
     unsigned u = 0;
     wchar_t c;
 
-    while( 0 != ( c = *str++ ) )
+    while( 0 != ( c = *wstr++ ) )
         u = u * 10 + c - '0';
 
     return u;
@@ -227,9 +246,9 @@ unsigned str2num( wchar_t * str )
 
 char bytesize_MB[] = "_____ MB";
 char bytesize_KB[] = "_____ KB";
-char bytesize_B[] = "_____ B";
+char bytesize_B[]  = "_____ B";
 
-char * num2bytesize( unsigned u )
+char * num2str_bs( unsigned u )
 {
     char * measure;
     char * str;
@@ -261,35 +280,36 @@ char * num2bytesize( unsigned u )
     return str;
 }
 
-unsigned strbreaker( wchar_t * str, wchar_t ** argv, unsigned max, wchar_t breaker, wchar_t quote )
+unsigned wstrbrk( wchar_t * wstr, wchar_t ** wstrs, unsigned max,
+                  wchar_t breaker, wchar_t quote )
 {
     wchar_t c;
     wchar_t is_begin = 0;
     wchar_t is_quote = 0;
-    unsigned argc = 0;
+    unsigned u = 0;
 
-    while( 0 != ( c = *str ) )
+    while( 0 != ( c = *wstr ) )
     {
         if( !is_begin )
         {
             if( c == breaker )
             {
-                str++;
+                wstr++;
                 continue;
             }
 
             else
             {
-                if( argc >= max )
+                if( u >= max )
                     return max;
 
                 is_begin = 1;
                 if( c == quote )
                 {
                     is_quote = 1;
-                    str++;
+                    wstr++;
                 }
-                argv[argc] = str;
+                wstrs[u] = wstr;
                 continue;
             }
         }
@@ -301,136 +321,167 @@ unsigned strbreaker( wchar_t * str, wchar_t ** argv, unsigned max, wchar_t break
 
             is_begin = 0;
             is_quote = 0;
-            *str = 0;
-            str++;
-            argc++;
+            *wstr = 0;
+            wstr++;
+            u++;
             continue;
         }
 
         if( is_quote )
         {
-            str++;
+            wstr++;
             continue;
         }
 
         if( c == breaker )
         {
             is_begin = 0;
-            *str = 0;
-            str++;
-            argc++;
+            *wstr = 0;
+            wstr++;
+            u++;
             continue;
         }
 
-        str++;
+        wstr++;
     }
 
     if( is_begin )
-        argc++;
+        u++;
 
-    return argc;
+    return u;
 }
 
-#define LOG_ERR_PROLOG  "ERROR: "
-#define LOG_ERR_BAD_CMD "Bad command line"
-#define LOG_ERR_WINHTTP "WinHTTP unknown error"
+#define LOG_ERR_PROLOG     "ERROR: "
+#define LOG_ERR_BAD_CMD    "Bad command line"
+#define LOG_ERR_WINHTTP    "WinHTTP unknown error"
 #define LOG_ERR_CREATEFILE "CreateFile failed"
-#define LOG_ERR_WRITEFILE "WriteFile failed"
-#define LOG_ERR_EPILOG  ".\r\n"
+#define LOG_ERR_WFILE      "WriteFile failed"
+#define LOG_ERR_EPILOG     ".\r\n"
 
-#define LOG_DOWNLOAD_PROLOG  "Downloading"
-#define LOG_DOWNLOAD_SIZE_PROLOG " "
-#define LOG_DOWNLOAD_EPILOG  ": .."
-#define LOG_DOWNLOADING  "."
-#define LOG_SUCCESS  " OK.\r\n"
+#define LOG_DLFL_PROLOG      "DLFL "
+#define LOG_DLFL_WORK_PROLOG "(.."
+#define LOG_DLFL_WORK        "."
+#define LOG_DLFL_WORK_EPILOG ")"
+#define LOG_DLFL_SUCCESS     " OK.\r\n"
 
-void strlogger( HANDLE hOut, char * prolog, char * str, char * epilog )
+void strlog( HANDLE hOut, unsigned count, ... )
 {
     if( hOut != INVALID_HANDLE_VALUE )
     {
-        DWORD dwTemp;
+        va_list strs;
+        va_start( strs, count );
 
-        if( prolog )
-            WriteFile( hOut, prolog, strlength( prolog ), &dwTemp, NULL );
-        if( str )
-            WriteFile( hOut, str, strlength( str ), &dwTemp, NULL );
-        if( epilog )
-            WriteFile( hOut, epilog, strlength( epilog ), &dwTemp, NULL );
+        for( ; count; count-- )
+        {
+            DWORD dw;
+            char * str = va_arg( strs, char * );
+
+            WriteFile( hOut, str, strlength( str ), &dw, NULL );
+        }
+
+        va_end( strs );
     }
 }
 
 void __cdecl mainCRTStartup()
 {
-    UINT code = 1;
-    DWORD dwTemp;
-    wchar_t * argv[3];
-    wchar_t * str[3];
-    unsigned counter;
-    unsigned quanter;
     winhttp_handler h;
-    HANDLE hFile = INVALID_HANDLE_VALUE;
-    HANDLE hOut = INVALID_HANDLE_VALUE;
-    UINT page;
+    unsigned        counter;
+    unsigned        quanter;
+    HANDLE          hFile = INVALID_HANDLE_VALUE;
+    HANDLE          hOut = INVALID_HANDLE_VALUE;
 
     hOut = GetStdHandle( STD_OUTPUT_HANDLE );
 
-    if( 3 != strbreaker( GetCommandLineW(), (wchar_t **)&argv, 3, ' ', '"' ) )
     {
-        strlogger( hOut, LOG_ERR_PROLOG, LOG_ERR_BAD_CMD, LOG_ERR_EPILOG );
-        goto end;
+        wchar_t * wstrs[3];
+
+        if( 3 != wstrbrk( GetCommandLineW(), (wchar_t **)&wstrs, 3, ' ', '"' ) )
+        {
+            strlog( hOut, 3, LOG_ERR_PROLOG, LOG_ERR_BAD_CMD, LOG_ERR_EPILOG );
+            goto end;
+        }
+
+        memzero( &h, sizeof( winhttp_handler ) );
+
+        h.url = wstrs[1];
+        h.isSSL = h.url[4] == 's';
+        h.filename = wstrs[2];
+
+        memcopy( h.bbChunk, wstrs[1], 2 * ( wstrs[2] - wstrs[1] ) );
     }
 
-    memzero( &h, sizeof( winhttp_handler ) );
-
-    h.url = argv[1];
-    h.isSSL = h.url[4] == 's';
-
-    memcopy( h.bbChunk, argv[1], ( argv[2] - argv[1] ) * sizeof( wchar_t ) );
-
-    if( 3 != strbreaker( (wchar_t *)h.bbChunk, (wchar_t **)&str, 3, '/', '#' ) )
     {
-        strlogger( hOut, LOG_ERR_PROLOG, LOG_ERR_BAD_CMD, LOG_ERR_EPILOG );
-        goto end;
+        wchar_t * wstrs[3];
+
+        if( 3 != 
+            wstrbrk( (wchar_t *)h.bbChunk, (wchar_t **)&wstrs, 3, '/', '#' ) )
+        {
+            strlog( hOut, 3, LOG_ERR_PROLOG, LOG_ERR_BAD_CMD, LOG_ERR_EPILOG );
+            goto end;
+        }
+
+        h.url_get = &h.url[wstrs[2] - wstrs[0] - 1];
+
+        counter = wstrbrk( wstrs[1], (wchar_t **)&wstrs, 2, ':', '#' );
+
+        h.server = wstrs[0];
+
+        if( counter == 1 )
+        {
+            h.port = h.isSSL ? 443 : 80; // default choice
+        }
+        else if( counter == 2 )
+        {
+            h.port = (INTERNET_PORT)wstr2num( wstrs[1] );
+        }
+        else
+        {
+            strlog( hOut, 3, LOG_ERR_PROLOG, LOG_ERR_BAD_CMD, LOG_ERR_EPILOG );
+            goto end;
+        }
     }
-
-    h.url_get = &h.url[str[2] - str[0] - 1];
-
-    counter = strbreaker( str[1], (wchar_t **)&str, 2, ':', '#' );
-
-    if( counter == 1 )
-        h.port = h.isSSL ? 443 : 80;
-    else if( counter == 2 )
-        h.port = (INTERNET_PORT)str2num( str[1] );
-    else
-    {
-        strlogger( hOut, LOG_ERR_PROLOG, LOG_ERR_BAD_CMD, LOG_ERR_EPILOG );
-        goto end;
-    }
-
-    h.server = str[0];
 
     counter = 0;
     quanter = 1024 * 1024 / 50 / sizeof( h.bbChunk ) + 1;
 
     for( ;; )
     {
-        dwTemp = winhttp_exec( &h );
+        DWORD dw = winhttp_exec( &h );
 
-        if( dwTemp != S_OK )
+        if( dw != S_OK )
         {
-            if( 0 != ( h.dwChunk = FormatMessageW( FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, NULL,
-                dwTemp, MAKELANGID( LANG_NEUTRAL, SUBLANG_DEFAULT ), (LPWSTR)h.bbChunk, sizeof( h.bbChunk ) / 2, NULL ) ) ||
-
-                0 != ( h.dwChunk = FormatMessageW( FORMAT_MESSAGE_FROM_HMODULE | FORMAT_MESSAGE_IGNORE_INSERTS, (VOID *)GetModuleHandleA( "winhttp.dll" ),
-                dwTemp, MAKELANGID( LANG_NEUTRAL, SUBLANG_DEFAULT ), (LPWSTR)h.bbChunk, sizeof( h.bbChunk ) / 2, NULL ) ) )
+            if(
+                ( h.dwChunk = 
+                  FormatMessageW( FORMAT_MESSAGE_FROM_SYSTEM |
+                                  FORMAT_MESSAGE_IGNORE_INSERTS, NULL, dw,
+                                  MAKELANGID( LANG_NEUTRAL, SUBLANG_DEFAULT ),
+                                  (LPWSTR)h.bbChunk, sizeof( h.bbChunk ) / 2,
+                                  NULL )
+                ) != S_OK
+                || 
+                ( h.dwChunk =
+                  FormatMessageW( FORMAT_MESSAGE_FROM_HMODULE |
+                                  FORMAT_MESSAGE_IGNORE_INSERTS,
+                                  (VOID *)GetModuleHandleA( "winhttp.dll" ), dw,
+                                  MAKELANGID( LANG_NEUTRAL, SUBLANG_DEFAULT ),
+                                  (LPWSTR)h.bbChunk, sizeof( h.bbChunk ) / 2,
+                                  NULL )
+                ) != S_OK
+              )
             {
-                if( h.dwChunk * 2 < sizeof( h.bbChunk ) &&
-                    WideCharToMultiByte( GetConsoleOutputCP(), 0, (LPWSTR)h.bbChunk, -1, (LPSTR)h.bbChunk + sizeof( h.bbChunk ) / 2, sizeof( h.bbChunk ) / 2, NULL, NULL ) )
-                    strlogger( hOut, LOG_ERR_PROLOG, h.bbChunk + sizeof( h.bbChunk ) / 2, NULL );
+                WideCharToMultiByte( GetConsoleOutputCP(), 0, (LPWSTR)h.bbChunk,
+                                     -1,
+                                     (LPSTR)h.bbChunk + sizeof( h.bbChunk ) / 2,
+                                     sizeof( h.bbChunk ) / 2, NULL, NULL );
+
+                strlog( hOut, 2, LOG_ERR_PROLOG,
+                        h.bbChunk + sizeof( h.bbChunk ) / 2 );
             }
             else
             {
-                strlogger( hOut, LOG_ERR_PROLOG, LOG_ERR_WINHTTP, LOG_ERR_EPILOG );
+                strlog( hOut, 3, LOG_ERR_PROLOG, LOG_ERR_WINHTTP,
+                        LOG_ERR_EPILOG );
             }
 
             goto end;
@@ -438,47 +489,63 @@ void __cdecl mainCRTStartup()
 
         if( hFile == INVALID_HANDLE_VALUE )
         {
-            hFile = CreateFileW( argv[2], GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, 0, NULL );
+            hFile = CreateFileW( h.filename, GENERIC_WRITE, 0, NULL,
+                                 CREATE_ALWAYS, 0, NULL );
 
             if( hFile == INVALID_HANDLE_VALUE )
             {
-                strlogger( hOut, LOG_ERR_PROLOG, LOG_ERR_CREATEFILE, LOG_ERR_EPILOG );
+                strlog( hOut, 3, LOG_ERR_PROLOG, LOG_ERR_CREATEFILE,
+                        LOG_ERR_EPILOG );
                 goto end;
             }
 
             if( h.dwTotal && h.dwTotal > 1024 * 1024 )
                 quanter = h.dwTotal / 50 / sizeof( h.bbChunk ) + 1;
 
-            strlogger( hOut, LOG_DOWNLOAD_PROLOG, NULL, NULL );
             if( h.dwTotal )
-                strlogger( hOut, LOG_DOWNLOAD_SIZE_PROLOG, num2bytesize( h.dwTotal ), NULL );
-            strlogger( hOut, NULL, NULL, LOG_DOWNLOAD_EPILOG );
+            {
+                strlog( hOut, 4, LOG_DLFL_PROLOG, num2str_bs( h.dwLoaded ), " ",
+                        LOG_DLFL_WORK_PROLOG );
+            }
+            else
+            {
+                strlog( hOut, 2, LOG_DLFL_PROLOG, LOG_DLFL_WORK_PROLOG );
+            }
         }
 
         if( h.dwChunk == 0 )
         {
             if( !h.dwTotal )
-                strlogger( hOut, LOG_DOWNLOAD_SIZE_PROLOG, num2bytesize( h.dwLoaded ), NULL );
+            {
+                strlog( hOut, 4, LOG_DLFL_WORK_EPILOG, " ", 
+                        num2str_bs( h.dwLoaded ), LOG_DLFL_SUCCESS );
+            } 
+            else
+            {
+                strlog( hOut, 2, LOG_DLFL_WORK_EPILOG, LOG_DLFL_SUCCESS );
+            }
 
-            strlogger( hOut, LOG_SUCCESS, NULL, NULL );
-            break;
+            CloseHandle( hFile );
+            ExitProcess( 0 ); // SUCCESS
         }
 
-        if( !WriteFile( hFile, h.bbChunk, h.dwChunk, &dwTemp, NULL ) || h.dwChunk != dwTemp )
+        if( FALSE == WriteFile( hFile, h.bbChunk, h.dwChunk, &dw, NULL ) ||
+            h.dwChunk != dw )
         {
-            strlogger( hOut, LOG_ERR_PROLOG, LOG_ERR_WRITEFILE, LOG_ERR_EPILOG );
+            strlog( hOut, 3, LOG_ERR_PROLOG, LOG_ERR_WFILE, LOG_ERR_EPILOG );
             goto end;
         }
 
         counter++;
 
         if( counter % quanter == 0 )
-            strlogger( hOut, LOG_DOWNLOADING, NULL, NULL );
+            strlog( hOut, 1, LOG_DLFL_WORK );
     }
-
-    code = 0;
 
 end:
 
-    ExitProcess( code );
+    if( hFile != INVALID_HANDLE_VALUE )
+        CloseHandle( hFile );
+
+    ExitProcess( 1 );
 }
